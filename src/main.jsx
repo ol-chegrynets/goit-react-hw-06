165c105bf8fa53dbb41020ba27e8eca852ab37b6
shura73 <ol.chegrynets@gmail.com>
2024-12-04 18:57:29 +0100

    add persistor 2

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import 'modern-normalize';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
